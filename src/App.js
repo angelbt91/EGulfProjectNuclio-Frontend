08ead6125d656f034ebd5a85c5a13b8ab7776a7f
Fran <fransanchezcostas@hotmail.com>
2021-06-13 04:14:37 +0200

    Added tabs under header as tabheader component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import './App.css';
 import Header from './components/header/header';
+import TabHeader from './components/tabheader/tabheader';
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <hr className="_separatorLine" size="1" width="100000 px" color="gray"></hr>
+      <hr className="_separatorLine1" size="1" width="20000 px" color="#D9D9D9"></hr>
+      <TabHeader />
+      <hr className="_separatorLine2" size="1" width="20000 px" color="#D9D9D9"></hr>
     </div>
   );
 }
---
+++ src/components/tabheader/tabheader.js
@@ -0,0 +1,28 @@
+import './tabheader.css'
+
+function Rosificacion(event){
+    event.target.style.color = '#DA4167';
+    event.target.style.border = "4px solid #DA4167";
+}
+
+function Blackificacion(event){
+    event.target.style.color = '#798591';
+    event.target.style.border = "0px"
+}
+
+const TabHeader = () => {
+
+    const optionsArray = ["PORTADA","FAVORITOS","CASA Y JARDIN","MODA","MOTOR","COLECCIONISMO","OFERTAS"];
+    return(
+        <div className = "_tabheadContainer">
+            {optionsArray.map((option)=> {
+                return (
+                    <div class="tab" onMouseEnter={Rosificacion} onMouseLeave={Blackificacion}>{option}</div>
+                )
+            })}
+        </div>
+    )
+}
+
+
+export default TabHeader;
