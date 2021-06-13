5ace7d6039335ecbea4490fe50a3f55966184268
Fran <fransanchezcostas@hotmail.com>
2021-06-13 04:23:36 +0200

    tabheader completed, needs little fix

--- src/components/tabheader/tabheader.js
+++ src/components/tabheader/tabheader.js
@@ -2,12 +2,12 @@ import './tabheader.css'
 
 function Rosificacion(event){
     event.target.style.color = '#DA4167';
-    event.target.style.border = "4px solid #DA4167";
+    event.target.style.borderBottom = "4px solid #DA4167";
 }
 
 function Blackificacion(event){
     event.target.style.color = '#798591';
-    event.target.style.border = "0px"
+    event.target.style.borderBottom = "0px";
 }
 
 const TabHeader = () => {
