cdccaea2f85e751db634cf8a2ca231a2c8e30f41
Mario Escudero <marioeproduccion@gmail.com>
2021-06-20 21:26:49 +0200

    CSS arreglado y quitado codigo comentado

--- src/components/card/card.js
+++ src/components/card/card.js
@@ -23,26 +23,3 @@ const Card = ({img, name, rating, initprice}) => {
 
 export default Card;
 
-/* 
-            <div className="main_banner">
-                <div className="banner_info"> 
-                    <p>{name}</p>
-                    <p>{rating}</p>
-                </div>
-                <div className="banner_bits">
-                    <p>{initprice}</p>
-                </div>
-
-            </div> */
-
-            /* <div className="card_container">
-            <div>
-                <img src={img} alt=""/>
-            </div>
-            
-            <div className="card_banner">
-                <p>{name}</p>
-                <p>{rating}</p>
-            </div>
-            
-        </div> */
