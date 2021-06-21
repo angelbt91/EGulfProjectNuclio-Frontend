0a556c8ec19367f0bd5f2305792c875e54cdfb75
albertmasats <albertmasats@gmail.com>
2021-06-21 17:25:30 +0200

    EG97-17 Import footer component into App.js

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Header from './components/header/header';
 import TabHeader from './components/tabheader/tabheader';
 import AdBanner from './components/adbanner/adbanner';
 import Roll from './components/roll/roll';
+import Footer from './components/footer/footer';
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
       <AdBanner />
       <Roll tittle="Anuncios"/>
       <Roll tittle="Televisores"/>
+      <Footer />
     </div>
   );
 }
