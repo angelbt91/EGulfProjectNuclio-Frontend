a43f11b45081c1131a9a26824bb61d0bfafea958
Clàudia <claudia.martinez081996@gmail.com>
2021-07-07 20:58:48 +0200

    paint from auctionsId

--- src/components/productGallery/productGallery.js
+++ src/components/productGallery/productGallery.js
@@ -2,28 +2,31 @@ import "./productGallery.css";
 import ProductsList from "../comp/product.json";
 import GalleryImage from "../galleryimage/galleryImage";
 import { useState } from "react";
-const ProductGallery =  () => {
-    const [images, setImages]  = useState(ProductsList[5].img);
-    return(
-        <div className="galleryContainer">
-            <div className="imgMasterContainer">
-                <img classname="principalImage" alt="producto" src={ProductsList[5].img[0]} ></img>
-            </div>
-            <div classname="mosaicImage">
-                <div className="rowContainerOne">
-                    {images.slice(0, 2).map((img) => (
-                        <GalleryImage    img={img}/>                        
-                    ))
-                    }
-                </div>
-                <div className="rowContainerTwo">
-                    {images.slice(2, 4).map((img) => (
-                        <GalleryImage    img={img}/>                       
-                    ))
-                    }
-                </div>
-            </div>
+
+const ProductGallery = ({ URLimages }) => {
+  const [images, setImages] = useState(ProductsList[5].img);
+  return (
+    <div className="galleryContainer">
+      <div className="imgMasterContainer">
+        <img
+          classname="principalImage"
+          alt="producto"
+          src={ProductsList[5].img[0]}
+        ></img>
+      </div>
+      <div classname="mosaicImage">
+        <div className="rowContainerOne">
+          {images.slice(0, 2).map((img) => (
+            <GalleryImage img={img} />
+          ))}
         </div>
-    );
+        <div className="rowContainerTwo">
+          {images.slice(2, 4).map((img) => (
+            <GalleryImage img={img} />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 };
 export default ProductGallery;
