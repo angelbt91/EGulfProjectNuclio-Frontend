1dd337a6ca39bba4d487c6d675139655aac389ba
Veronica43 <82869608+Veronica43@users.noreply.github.com>
2021-07-07 20:48:06 +0200

    EG97-44 maquetado formulario

---
+++ src/pages/productformpage/productformpage.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ProductForm from "../../components/productform/productform";
+
+function ProductFormPage() {
+  return (
+    <div>
+      <ProductForm />
+    </div>
+  );
+}
+
+export default ProductFormPage;
