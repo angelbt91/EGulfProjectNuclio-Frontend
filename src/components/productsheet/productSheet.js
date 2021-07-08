3f558efa2857c48e47f5d588c299269e6018165f
albertmasats <albertmasats@gmail.com>
2021-07-08 22:47:33 +0200

    EG97-24 Make bids functional ( in progress)

--- src/components/productsheet/productSheet.js
+++ src/components/productsheet/productSheet.js
@@ -1,50 +1,95 @@
-import BidButton from "../bidbutton/bidButton"
-import TitleProduct from "../titleproduct/titleProduct"
-import "./productSheet.css"
+import BidButton from "../bidbutton/bidButton";
+import TitleProduct from "../titleproduct/titleProduct";
+import "./productSheet.css";
+import { useState } from "react";
 
+const ProductSheet = ({
+  id,
+  title,
+  description,
+  initprice,
+  iduser,
+  rating,
+}) => {
+  const [bidAmount, setbidAmount] = useState(initprice);
+  const handleChange = (event) => {
+    setbidAmount(event.target.value);
+  };
 
-const ProductSheet = ({title,description,initprice,iduser,rating}) => {
-   
-    return(
-        <div className="productContainerGeneral">
-            <div className="productSheetContainer">
-                <TitleProduct title={title}/>
-                <hr className="productSheetSeparator"></hr>
-                <div className="descriptionSheetContainer">
-                    <h2 className= "descriptionTitle">Description</h2>
-                    <p className="descriptionProductSheet">{description}</p>
-                    <h3 className= "descriptionShipping">Envío y condiciones</h3>
-                    <h3 className= "descriptionShipping">Prioritario UE <h3 className="price">18€</h3></h3>
-                </div>    
-            </div> 
-            <div className="businessContainer">
-                <div className="bildsContainer">
-                    <div className="businessContainerInside">
-                        <p className="priceActuality">{initprice} EUR </p>
-                        <p className="bidActuality"> 2 Puja</p>
-                        <input className="inputBid" type="text" name="textBid" placeholder="Introduce tu puja maxima"></input>
-                        <div className="placeButton">
-                            <BidButton />
-                        </div>
-                    </div>
-                </div>
-                <div className="sellerContainer">
-                    <div className="businessContainerInside">
-                        <TitleProduct title="Información del vendedor"/>
-                        <p className="userActuality">{iduser}</p>
-                        <p className="ratingUser">(  {rating}%)</p>
-                        <div className="linkContainer">
-                            <p className="linkOther">Ver otros artículos</p>
-                            <p className="linkOther">Guardar como favorito</p>
-                        </div>
-                        
-                        
-                    </div>    
-                </div>
-            </div>    
-            
+  const body = { bidAmount };
+  const localStorageToken = localStorage.getItem("token");
+
+  const handleClick = () => {
+    fetch(`http://localhost:5001/products/${id}/currentAuction/bid`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${localStorageToken}`,
+      },
+      body: JSON.stringify(body),
+    }).then(() => {
+      fetch(`http://localhost:5001//${id}/currentAuction`)
+        .then((res) => {
+          if (res.ok) {
+            console.log("OK!");
+            throw res.json();
+          } else {
+            throw res.text();
+          }
+        })
+        .then((response) => {
+          return response.json();
+        })
+        .then((myJson) => {
+          console.log(myJson);
+        });
+    });
+  };
+
+  return (
+    <div className="productContainerGeneral">
+      <div className="productSheetContainer">
+        <TitleProduct title={title} />
+        <hr className="productSheetSeparator"></hr>
+        <div className="descriptionSheetContainer">
+          <h2 className="descriptionTitle">Description</h2>
+          <p className="descriptionProductSheet">{description}</p>
+          <h3 className="descriptionShipping">Envío y condiciones</h3>
+          <h3 className="descriptionShipping">
+            Prioritario UE <h3 className="price">18€</h3>
+          </h3>
         </div>
-        
-    )
-}
+      </div>
+      <div className="businessContainer">
+        <div className="bildsContainer">
+          <div className="businessContainerInside">
+            <p className="priceActuality">{bidAmount} EUR </p>
+            <p className="bidActuality"> 2 Puja</p>
+            <input
+              className="inputBid"
+              type="text"
+              name="textBid"
+              placeholder="Introduce tu puja"
+              onChange={handleChange}
+            ></input>
+            <div className="placeButton">
+              <BidButton savePrice={handleClick} />
+            </div>
+          </div>
+        </div>
+        <div className="sellerContainer">
+          <div className="businessContainerInside">
+            <TitleProduct title="Información del vendedor" />
+            <p className="userActuality">{iduser}</p>
+            <p className="ratingUser">( {rating}%)</p>
+            <div className="linkContainer">
+              <p className="linkOther">Ver otros artículos</p>
+              <p className="linkOther">Guardar como favorito</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 export default ProductSheet;
