04cbc0f7dc76d5e2995be6600278d754a4f34140
Federico Vélez <federico.velezferro@gmai.com>
2021-07-12 21:11:38 +0200

    CSS Fix

--- src/components/tabheader/tabheader.js
+++ src/components/tabheader/tabheader.js
@@ -15,33 +15,34 @@ const TabHeader = () => {
   
   const history = useHistory();
  
-  function ShowTabSubheader() {
+  
     const [isShown,setIsShown] = useState(false);
 
     return (
-      <div className="_tabheadContainer">
-        <div>
-          <span onClick={() => history.push("/")}>PORTADA</span>
-        </div>
-        <div>
-          <span onClick={() => history.push("/favouritePage")}>FAVORITOS</span>
-        </div>
+      <div>
+        <div className="_tabheadContainer">
+          <div>
+            <span onClick={() => history.push("/")}>PORTADA</span>
+          </div>
+          <div>
+            <span onClick={() => history.push("/favouritePage")}>FAVORITOS</span>
+          </div>
 
-        {optionsArray.map((option) => {
-          return (
-            <div>
-              <div onMouseEnter={() => setIsShown(true)} onMouseLeave={() => setIsShown(false)} className="tab">
-                <span onClick= {() => history.push("/favouritePage")} id="notthediv">{option.toLowerCase()}</span>
-              </div>
-              {isShown && 
-                <div>
-                  <TabSubHeader />
+          {optionsArray.map((option) => {
+            return (
+              <div>
+                <div onMouseEnter={() => setIsShown(true)} onMouseLeave={() => setIsShown(false)} className="tab">
+                  <span onClick= {() => history.push("/favouritePage")} id="notthediv">{option.toLowerCase()}</span>
                 </div>
-              }
-            </div>
-        )})};
+              </div>
+          )})}
+        </div>
+        {isShown && 
+                  <div>
+                    <TabSubHeader />
+                  </div>
+                }
       </div>
-      ); 
-  }    
+    );   
 };
 export default TabHeader
