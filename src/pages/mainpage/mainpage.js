96346b58e1d9888e3826744baca20e8f4d92a20b
albertmasats <albertmasats@gmail.com>
2021-07-12 17:17:59 +0200

    EG97-24 Comment for working development

--- src/pages/mainpage/mainpage.js
+++ src/pages/mainpage/mainpage.js
@@ -2,13 +2,13 @@ import "./mainpage.css";
 import TabHeader from "../../components/tabheader/tabheader";
 import AdBanner from "../../components/adbanner/adbanner";
 import Roll from "../../components/roll/roll";
-import SubHeader from "../../components/tabheader/subheader/subheader";
+//import SubHeader from "../../components/tabheader/subheader/subheader";
 
 const MainPage = () => {
   return (
     <div className="App">
       <TabHeader />
-      <SubHeader />
+      {/* <SubHeader /> */}
       <hr className="_separatorLine2" size="1" width="99.9%" color="#D9D9D9" />
       <AdBanner />
       <Roll title="Anuncios" />
