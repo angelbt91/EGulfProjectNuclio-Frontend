5d9c660c1d171add15948a3d3cd057dd823c3b47
Veronica43 <82869608+Veronica43@users.noreply.github.com>
2021-07-16 19:53:39 +0200

    EG97-55

--- src/components/roll/roll.js
+++ src/components/roll/roll.js
@@ -2,28 +2,10 @@ import Card from "../card/card";
 import ProductsList from "../comp/product.json";
 import "./roll.css";
 import { useEffect, useState } from "react";
+import { useAuctions } from "../../hooks/useAuctions";
 
-const Roll = ({ title, link }) => {
-  const url = "http://localhost:5001/auctions";
-  const [auctions, setAuctions] = useState(undefined);
-
-  useEffect(() => {
-    fetch(url)
-      .then((response) => {
-        if (response.ok) {
-          return response.json();
-        } else {
-          throw Error(response.statusText);
-        }
-      })
-      .then((data) => {
-        let auctionUser = data;
-        setAuctions(auctionUser);
-      })
-      .catch((error) => {
-        console.error(error);
-      });
-  }, []);
+const Roll = ({ title }) => {
+  const { auctions } = useAuctions();
 
   return (
     <div className="main_roll">
@@ -35,12 +17,12 @@ const Roll = ({ title, link }) => {
             .slice(0, 4)
             .map((auction) => (
               <Card
-                img={auction.productId.images[0]}
-                name={auction.productId.name}
-                rating={auction.productId.owner.rating}
+                img={auction.productId?.images[0]}
+                name={auction.productId?.name}
+                rating={auction.productId?.owner.rating}
                 initprice={auction.startingPrice}
-                id={auction.productId._id}
-                nameUser={auction.productId.owner.name}
+                id={auction.productId?._id}
+                nameUser={auction.productId?.owner.name}
               />
             ))}
       </div>
