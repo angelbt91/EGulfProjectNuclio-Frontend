9234f7c7037303dd6c86aa34beb80cf6496b5a48
FedericoVelez25 <federico.velezferro@gmail.com>
2021-07-19 21:42:01 +0200

    CSS FIX

--- src/components/tabheader/tabheader.js
+++ src/components/tabheader/tabheader.js
@@ -1,49 +1,83 @@
 import "./tabheader.css";
 import { useHistory } from "react-router-dom";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import TabSubHeader from "../tabsubheader/tabsubheader";
-
-const TabHeader = () => {
-  const optionsArray = [
-    "CASA Y JARDIN",
-    "MODA",
-    "MOTOR",
-    "COLECCIONISMO",
-    "OFERTAS",
-  ];
-  
-  const history = useHistory();
- 
-  
-    const [isShown,setIsShown] = useState(false);
-
-    return (
-      <div>
-        <div className="_tabheadContainer">
-          <div>
-            <span onClick={() => history.push("/")}>PORTADA</span>
-          </div>
-          <div>
-            <span onClick={() => history.push("/favouritePage")}>FAVORITOS</span>
-          </div>
-        <div onMouseLeave={() => setIsShown(false)} className="_tabheadContainer">
-
-          {optionsArray.map((option) => {
-            return (
-              <div>
-                <div onMouseEnter={() => setIsShown(true)} className="tab">
-                  <span onClick= {() => history.push("/favouritePage")} id="notthediv">{option.toLowerCase()}</span>
-                </div>
-              </div>
-          )})}
-          {isShown && 
-                  <div>
-                    <TabSubHeader />
-                  </div>
-                }
-          </div>
-        </div>
-      </div>
-    );   
+const agregateCategoriesAndSubcategories = (categoryList) => {
+  const filteredCategories = {};
+  const filteredSubcategories = {};
+  categoryList.forEach((category) => {
+    const hasCategoryParent =
+      category.parentCategory && category.parentCategory !== "";
+    if (!hasCategoryParent) {
+      return (filteredCategories[category.name] = [category._id]);
+    } else {
+      return (filteredSubcategories[category.name] = [category.parentCategory]);
+    }
+  });
+  let finalCategories = {};
+  Object.keys(filteredCategories).forEach((category) => {
+    const categoryToSearch = filteredCategories[category][0];
+    const subcategories = Object.keys(filteredSubcategories).filter(
+      (subcategory) =>
+        filteredSubcategories[subcategory][0] === categoryToSearch
+    );
+    finalCategories[category] = subcategories;
+  });
+  return finalCategories;
 };
-export default TabHeader
+const TabHeader = () => {
+  const [categories, setCategories] = useState();
+  const history = useHistory();
+  const [selectedCategory, setSelectedCategory] = useState(false);
+  useEffect(() => {
+    fetch(`http://localhost:5001/categories`)
+      .then((response) => response.json())
+      .then((json) => {
+        setCategories(agregateCategoriesAndSubcategories(json));
+      });
+  }, []);
+  console.log(selectedCategory);
+  return (
+    <div>
+      <div
+        onMouseLeave={() => setSelectedCategory(null)}
+        className="_headContainer"
+      >
+        <div className="_tabheadContainer">
+          <span onClick={() => history.push("/")}>PORTADA</span>
+        </div>
+        <div className="_tabheadContainer">
+          <span onClick={() => history.push("/favouritePage")}>FAVORITOS</span>
+        </div>
+        {categories &&
+          Object.keys(categories).map((category) => {
+            return (
+              <div
+                className="_tabheadContainer"
+                onMouseEnter={() => setSelectedCategory(category)}
+              >
+                <span>{category}</span>
+              </div>
+            );
+          })}
+        {console.log(categories && categories.selectedCategory)}
+        {selectedCategory && (
+          <div>
+            <TabSubHeader
+              subcategories={
+                categories &&
+                categories[selectedCategory] &&
+                categories[selectedCategory].map((subcategory) => {
+                  return (
+                    <div className="_tabSubheadContainer">{subcategory}</div>
+                  );
+                })
+              }
+            />
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
+export default TabHeader;
--- src/components/tabsubheader/tabsubheader.js
+++ src/components/tabsubheader/tabsubheader.js
@@ -1,34 +1,11 @@
 import "./tabsubheader.css";
-import { useState } from "react";
-import { useHistory } from "react-router-dom";
-
-const TabSubHeader = () => {
-  const optionsArray = [
-    "ZAPATOS",
-    "ABRIGOS",
-    "VESTIDOS",
-    "TRAJES",
-    "ACCESORIOS",
-    "BAÑO",
-    "JERSÉIS",
-  ];
-
-  const history = useHistory();
-  
-    return (
-      <div>
-        <div className="_tabsubheadContainer">
-          {optionsArray.map((option) => {
-            return (
-              <div>
-                <span onClick= {() => history.push("/favouritePage")} id="notthediv">{option.charAt(0).toUpperCase() + option.slice(1).toLowerCase()}</span>
-              </div>
-            );
-          })}
-        </div>
+const TabSubHeader = ({ subcategories }) => {
+  return (
+    <div className="_subheadContainerPosition">
+      <div className="_subheadContainer">
+        <span className="_tabSubheadContainer">{subcategories}</span>
       </div>
-    );
+    </div>
+  );
 };
-
-
 export default TabSubHeader;
