fd8c2b034981a66e933c819c6b2818740cfab99a
albertmasats <albertmasats@gmail.com>
2021-07-22 21:20:14 +0200

    EG97-30 delete code now in useAuctions hook

--- src/components/roll/roll.js
+++ src/components/roll/roll.js
@@ -5,31 +5,6 @@ import { useEffect, useState } from "react";
 import { useAuctions } from "../../hooks/useAuctions";
 
 const Roll = ({ title }) => {
-  const url = "http://localhost:5001/auctions";
-  const [auctions, setAuctions] = useState(undefined);
-
-  useEffect(() => {
-    fetch(url)
-      .then((response) => {
-        if (response.ok) {
-          return response.json();
-        } else {
-          throw Error(response.statusText);
-        }
-      })
-      .then((data) => {
-        let auctionUser = data;
-        setAuctions(auctionUser);
-      })
-      .catch((error) => {
-        console.error(error);
-      });
-  }, []);
-
-  useEffect(() => {
-    fetch(url);
-  }, []);
-
   const { auctions } = useAuctions("60e7f052df5a6d070e1ad960");
   return (
     <div className="main_roll">
@@ -47,7 +22,6 @@ const Roll = ({ title }) => {
                 initprice={auction.startingPrice}
                 id={auction._id}
                 nameUser={auction.productId.owner.name}
-                usersFavs={auction.productId.userFavs}
               />
             ))}
       </div>
