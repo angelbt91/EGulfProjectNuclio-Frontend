43fd498e04641b776eef7bbbf4500e20101126ca
Veronica43 <82869608+Veronica43@users.noreply.github.com>
2021-07-27 19:03:33 +0200

    remove old files

--- src/pages/mainpage/mainpage.js
+++ src/pages/mainpage/mainpage.js
@@ -3,13 +3,12 @@ import TabHeader from "../../components/tabheader/tabheader";
 import TabSubHeader from "../../components/tabsubheader/tabsubheader";
 import AdBanner from "../../components/adbanner/adbanner";
 import Roll from "../../components/roll/roll";
-// import SubHeader from "../../components/tabheader/subheader/subheader";
 
 const MainPage = () => {
   return (
     <div className="App">
       <TabHeader />
-      {/* {<SubHeader />} */}
+
       <hr className="_separatorLine2" size="1" width="99.9%" color="#D9D9D9" />
 
       <AdBanner />
