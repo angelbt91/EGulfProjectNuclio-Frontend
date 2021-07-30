9c739749e43ddcbd783fe43b92df3b9536f2e7e7
albertmasats <albertmasats@gmail.com>
2021-07-30 20:57:46 +0200

    change apiHost

--- src/utils/apiHost/apiHost.js
+++ src/utils/apiHost/apiHost.js
@@ -1,13 +1,4 @@
-let backendHost;
 let headerColor;
-const hostname = window && window.location && window.location.hostname;
-if (hostname === "localhost") {
-  backendHost = "http://127.0.0.1:5000/";
-  headerColor = "#1eac00";
-} else if (hostname === "egulfbackend.herokuapp.com/") {
-  backendHost = "https://egulfbackend.herokuapp.com/";
-  headerColor = "#1890ff";
-} else backendHost = "http://127.0.0.1:5000/";
-export const API_ROOT = `${backendHost}`;
-export const HOSTNAME = `${hostname}`;
+
+export const API_ROOT = `${process.env.API_ROOT}`;
 export const HEADER_COLOR = `${headerColor}`;
