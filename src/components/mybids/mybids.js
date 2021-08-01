49d4d9c7c8836e6c77eaa873936586c13ae5b7ac
Veronica43 <82869608+Veronica43@users.noreply.github.com>
2021-08-01 17:15:17 +0200

    mybidspage

--- src/components/mybids/mybids.js
+++ src/components/mybids/mybids.js
@@ -16,24 +16,31 @@ const MyBids = () => {
       </div>
       <div className="subastas">
         <p className="Sub_ganado">Ganando</p>
-
         <span className="tu_puja">Tu puja</span>
-
         <span className="puja_maxima">Puja máxima</span>
         <span className="acaba_en">Acaba en:</span>
         <br></br>
         <span className="price_pujamaxima">67€</span>
         <span className="price_tupuja">67€</span>
-        <span className="fin_puja">17/09/2021</span>
+        <span className="fin_puja">2d 09h 20m</span>
 
         <p className="Sub_activas_perdiendo">Subastas activas perdiendo</p>
         <span className="tu_puja">Tu puja</span>
         <span className="puja_maxima">Puja máxima</span>
         <span className="acaba_en">Acaba en:</span>
+        <br></br>
+        <span className="price_pujamaxima">100€</span>
+        <span className="price_tupuja">67€</span>
+        <span className="fin_puja">1d 09h 20m</span>
+
         <p className="Sub_activas_perdidas">Subastas activas perdidas</p>
         <span className="tu_puja">Tu puja</span>
         <span className="puja_maxima">Puja máxima</span>
-        <span className="acaba_en">Acaba en:</span>
+        <span className="acaba_en">Acabo el:</span>
+        <br></br>
+        <span className="price_pujamaxima">100€</span>
+        <span className="price_tupuja">55€</span>
+        <span className="fin_puja">17/09/2021</span>
       </div>
     </div>
   );
