e7c327c44b63a0171bc332f12a251107eac97b89
albertmasats <albertmasats@gmail.com>
2021-08-03 21:09:05 +0200

    EG97-64 create registry form and style

--- src/components/signupform/signupform.js
+++ src/components/signupform/signupform.js
@@ -38,43 +38,63 @@ const SignUp = ({ login }) => {
   };
 
   return (
-    <div className="photo-form-container">
+    <div className="photoform_container">
       <div className="mainPhoto">
         <img
           src={mainPhoto}
-          className="main-photo"
+          className="main_photo"
           alt="photo of different products"
+          width="550px"
         />
       </div>
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <input {...register("name")} />
-        <label htmlFor="email">email</label>
+      <form className="registry_form" onSubmit={handleSubmit(onSubmit)}>
+        <label htmlFor="name"></label>
         <input
+          placeholder="Nombre y apellidos"
+          id="name"
+          {...register("name", {
+            required: "requerido",
+          })}
+          type="text"
+        />
+        {errors.name && <span role="alert">{errors.name.message}</span>}
+        <label htmlFor="email"></label>
+        <input
+          placeholder="Correo electrónico"
           id="email"
           {...register("email", {
-            required: "required",
+            required: "requerido",
             pattern: {
               value: /\S+@\S+\.\S+/,
-              message: "Los valores introducidos no coinciden con un email",
+              message: "Introduzca una dirección de correo electrónico válida",
             },
           })}
           type="email"
         />
         {errors.email && <span role="alert">{errors.email.message}</span>}
-        <label htmlFor="password">password</label>
+        <label htmlFor="password"></label>
         <input
+          placeholder="Constraseña"
           id="password"
           {...register("password", {
-            required: "required",
+            required: "requerido",
             minLength: {
               value: 6,
-              message: "el minimo son 6",
+              message: "debe contener al menos 6 caracteres",
             },
           })}
           type="password"
         />
         {errors.password && <span role="alert">{errors.password.message}</span>}
-        <button type="submit">SUBMIT</button>
+        <div className="text_container">
+          <p>Al pulsar Regístrate, confirmas haber leído y </p>
+          <p>aceptado nuestras Condiciones de uso. Para </p>
+          <p>más información sobre el tratamiento de tus </p>
+          <p>datos, consulta nuestro Aviso de privacidad </p>
+        </div>
+        <button type="submit" className="submit_registry">
+          Regístrate
+        </button>
       </form>
     </div>
   );
