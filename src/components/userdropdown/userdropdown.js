f30fb68ca155b359f3f92afb67f641d49814899b
Veronica43 <82869608+Veronica43@users.noreply.github.com>
2021-08-04 15:35:17 +0200

    OK

--- src/components/userdropdown/userdropdown.js
+++ src/components/userdropdown/userdropdown.js
@@ -1,44 +1,69 @@
 import "./userdropdown.css";
 import UserAvatar from "../../assets/UserAvatar.png";
-import { useState } from "react";
-import {
-  Link,
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  useHistory,
-} from "react-router-dom";
+import { useState, useEffect } from "react";
+import { useHistory } from "react-router-dom";
+import { API_ROOT } from "../../utils/apiHost/apiHost";
 
 const UserDropdown = ({ user }) => {
   const history = useHistory();
-  const url = "/me/mybidspage";
+
   const [showDropdown, setShowDropdown] = useState(false);
+
+  useEffect(() => {
+    fetch(`${API_ROOT}users/userId`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: "Bearer " + localStorage.token,
+      },
+    })
+      .then((response) => {
+        if (response.ok) {
+          return response.json();
+        } else {
+          throw Error(response.statusText);
+        }
+      })
+      .then(() => {});
+  }, []);
+
   return (
-    <Router>
-      <div className="userDropdown_container">
-        <div
-          className="userDropdown_anchor"
-          onClick={() => setShowDropdown(!showDropdown)}
-        >
-          <img
-            src={UserAvatar}
-            alt="userPicture"
-            className="userAvatar_container"
-          />
-        </div>
-        {showDropdown && (
-          <div className="userDropdown_items">
-            <Link to={url} onClick={() => history.push("/")}>
-              <div className="dropdown_item">Home</div>
-            </Link>
-            <Link to={url} onClick={() => history.push("/me/mybidspage")}>
-              <div className="dropdown_item">My bids</div>
-            </Link>
-            <div className="dropdown_item">Log out</div>
-          </div>
-        )}
+    <div className="userDropdown_container">
+      <div
+        className="userDropdown_anchor"
+        onClick={() => setShowDropdown(!showDropdown)}
+      >
+        <img
+          src={UserAvatar}
+          alt="userPicture"
+          className="userAvatar_container"
+        />
       </div>
-    </Router>
+      {showDropdown && (
+        <div className="userDropdown_items">
+          <div className="dropdown_item" onClick={() => history.push("/")}>
+            Home
+          </div>
+
+          <div
+            className="dropdown_item"
+            onClick={() => history.push("/me/mybidspage")}
+          >
+            My bids
+          </div>
+
+          <div
+            className="dropdown_item"
+            onClick={() => {
+              localStorage.setItem("token", "");
+              history.push("/");
+            }}
+          >
+            Log out
+          </div>
+        </div>
+      )}
+    </div>
   );
 };
 export default UserDropdown;
