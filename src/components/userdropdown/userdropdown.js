0a3a0de46a72f66a190e70f673a10f61ac097759
Veronica43 <82869608+Veronica43@users.noreply.github.com>
2021-08-04 14:59:10 +0200

    header-avatar

--- src/components/userdropdown/userdropdown.js
+++ src/components/userdropdown/userdropdown.js
@@ -1,28 +1,44 @@
 import "./userdropdown.css";
 import UserAvatar from "../../assets/UserAvatar.png";
 import { useState } from "react";
+import {
+  Link,
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  useHistory,
+} from "react-router-dom";
+
 const UserDropdown = ({ user }) => {
+  const history = useHistory();
+  const url = "/me/mybidspage";
   const [showDropdown, setShowDropdown] = useState(false);
   return (
-    <div className="userDropdown_container">
-      <div
-        className="userDropdown_anchor"
-        onClick={() => setShowDropdown(!showDropdown)}
-      >
-        <img
-          src={UserAvatar}
-          alt="userPicture"
-          className="userAvatar_container"
-        />
-      </div>
-      {showDropdown && (
-        <div className="userDropdown_items">
-          <div className="dropdown_item">Profile</div>
-          <div className="dropdown_item">My bids</div>
-          <div className="dropdown_item">Log out</div>
+    <Router>
+      <div className="userDropdown_container">
+        <div
+          className="userDropdown_anchor"
+          onClick={() => setShowDropdown(!showDropdown)}
+        >
+          <img
+            src={UserAvatar}
+            alt="userPicture"
+            className="userAvatar_container"
+          />
         </div>
-      )}
-    </div>
+        {showDropdown && (
+          <div className="userDropdown_items">
+            <Link to={url} onClick={() => history.push("/")}>
+              <div className="dropdown_item">Home</div>
+            </Link>
+            <Link to={url} onClick={() => history.push("/me/mybidspage")}>
+              <div className="dropdown_item">My bids</div>
+            </Link>
+            <div className="dropdown_item">Log out</div>
+          </div>
+        )}
+      </div>
+    </Router>
   );
 };
 export default UserDropdown;
