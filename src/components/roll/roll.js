0060ddc75b6822b91441c739f027a8dad44d1e6a
Veronica43 <82869608+Veronica43@users.noreply.github.com>
2021-08-05 11:42:11 +0200

    fix css

--- src/components/roll/roll.js
+++ src/components/roll/roll.js
@@ -8,8 +8,8 @@ import { useAuctions } from "../../hooks/useAuctions";
 const Roll = ({ title }) => {
   const { auctions } = useAuctions("60e7f052df5a6d070e1ad960");
   return (
-    <div className="container_card_roll">
-      <h3 className="titulo_principal">{title}</h3>
+    <div className="main_roll">
+      <h3>{title}</h3>
       <div className="card_roll">
         {auctions &&
           auctions
