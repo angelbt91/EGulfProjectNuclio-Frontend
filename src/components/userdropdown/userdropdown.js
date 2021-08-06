f4a30f16dd7dc7a49ff89efdb0f93020cf55f565
Veronica43 <82869608+Veronica43@users.noreply.github.com>
2021-08-06 15:55:29 +0200

    EG97-header-avatar

--- src/components/userdropdown/userdropdown.js
+++ src/components/userdropdown/userdropdown.js
@@ -6,41 +6,9 @@ import { API_ROOT } from "../../utils/apiHost/apiHost";
 
 const UserDropdown = ({ user }) => {
   const history = useHistory();
-  // const [refresh, setRefresh] = useState();
+
   const [showDropdown, setShowDropdown] = useState(false);
 
-  // setRefresh(localStorage.getItem("token"));
-
-  /* const probeToken = () => {
-    if (token === "") {
-      setRefresh(false);
-    } else {
-      setRefresh(true);
-    }
-  }; */
-
-  /* useEffect(() => {
-    window.location.reload();
-  }, [refresh]); */
-
-  /*  useEffect(() => {
-    fetch(`${API_ROOT}users/userId`, {
-      method: "GET",
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: "Bearer " + localStorage.token,
-      },
-    })
-      .then((response) => {
-        if (response.ok) {
-          return response.json();
-        } else {
-          throw Error(response.statusText);
-        }
-      })
-      .then(() => {});
-  }, []);
- */
   return (
     <div className="userDropdown_container">
       {localStorage.getItem("token") !== "" && (
